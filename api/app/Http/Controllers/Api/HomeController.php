22dc5d1535a7f1edd9333380d7e08ebf4849796d
narekrubenyan <narek.rubenyan@gmail.com>
2023-08-18 17:56:58 +0400

    api update

--- api/app/Http/Controllers/Api/HomeController.php
+++ api/app/Http/Controllers/Api/HomeController.php
@@ -12,7 +12,96 @@ class HomeController extends Controller
     public function getHeaderItems() : JsonResponse
     {
 
-        $categories = Category::with('subcategories.product.translations')->get();
+        $categories = Category::with(
+            'translations',
+            'subcategories.translations',
+            'subcategories.product.images',
+            'subcategories.product.translations'
+        )->get();
+
+        $dataCategoryes = [];
+
+        foreach ($categories as $cat) {
+
+            $thisCategory = [
+                'name' => $cat->name,
+                'image' => $cat->image
+            ];
+
+            if (isset($cat->translations)) {
+                $thisCategory['translations'] = [];
+
+                foreach ( $cat->translations as $tr) {
+                    $thisCategory['translations'] += [$tr->language => $tr->name];
+                }
+            }
+
+            if (isset($cat->subcategories)) {
+
+                $thisCategory['subcategories'] = [];
+
+                foreach ( $cat->subcategories as $sub) {
+                    $thisSub = [
+                        'name' => $sub->name,
+                        'image' => $sub->image
+                    ];
+
+                    if (isset($sub->translations)) {
+                        $thisSub['translations'] = [];
+
+                        foreach ( $sub->translations as $tr) {
+                            $thisSub['translations'] += [$tr->language => $tr->name];
+                        }
+                    }
+
+                    if (isset($sub->product)) {
+
+                        $thisSub['products'] = [];
+
+                        foreach ( $sub->product as $product) {
+
+                            $thisProduct = [
+                                'name' => $product->name,
+                                "price" => $product->price,
+                                "description" => $product->description,
+                                "count" => $product->count,
+                            ];
+
+                            if (isset($product->translations)) {
+                                $thisProduct['translations'] = [];
+
+                                foreach ( $product->translations as $tr) {
+
+                                    $thisProductTr[$tr->language] = [
+                                        'name' => $tr->name,
+                                        'description' => $tr->description
+                                    ];
+
+                                    $thisProduct['translations'] += $thisProductTr;
+                                }
+                            }
+
+                            if (isset($product->images)) {
+
+                                $thisProduct['images'] = [];
+
+                                foreach ($product->images as $image) {
+                                    array_push($thisProduct['images'], $image->filename);
+                                }
+
+
+                            }
+
+                            $thisSub['products'] += $thisProduct;
+                        }
+                    }
+
+                    array_push($thisCategory['subcategories'], $thisSub);
+                }
+            }
+
+            array_push($dataCategoryes, $thisCategory);
+        }
 
         $data = [
             [
@@ -46,7 +135,7 @@ class HomeController extends Controller
                     "am" => "",
                     "en" => "products"
                 ],
-                'categories' => $categories->toArray()
+                'categories' => $dataCategoryes
             ],
         ];
 
